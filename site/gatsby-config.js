967b77f66cbe30722fdbb42c9d1cda31f2b7e066
Adam Crockett <adam.crockett@incorta.com>
2021-10-11 11:52:15 -0700

    shared theme creation

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,5 +1,19 @@
 module.exports = {
+    siteMetadata: {
+        title: 'My Book Club',
+        navItems: [
+            {
+                label: 'Books',
+                path:"/books",
+            },
+            {
+                label: 'Authors',
+                path:"/authors",
+            },
+        ],
+    },
     plugins: [
+        'gatsby-theme-shared-nav',
         'gatsby-plugin-image',
         'gatsby-plugin-sharp',
         'gatsby-transformer-sharp',
